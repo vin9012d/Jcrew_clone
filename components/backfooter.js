fa4ce8017028f1c23e8c3564a1bf138e856e50c5
kishan169 <kishansavaj1206@gmail.com>
2022-06-15 10:27:46 +0530

    homepage

---
+++ components/backfooter.js
@@ -0,0 +1,84 @@
+function footer(){
+    return `
+    <div id = "footer">
+    <div id = "content">
+        <div id = "1stline">
+            <p>Help</p>
+            <p>Customer Service</p>
+            <p>Track Order</p>
+            <p>Return & Exchanges</p>
+            <p>Shipping</p>
+            <p>International Orders</p>
+            <p>Contact Us</p>
+        </div>
+
+        <div id = "2ndline">
+            <p>Quick Links</p>
+            <p>Size Charts</p>
+            <p>Offers & Promotions</p>
+        </div>
+
+        <div id = "3rdLine">
+            <p>About J.crew</p>
+            <p>Our Story</p>
+            <p>Career</p>
+            <p>Social Responsibility</p>
+            <p>California Transparency Act/Modern Slavery Act</p>
+            <p>Investor Relation</p>
+            <p>Terms Of Use</p>
+            <p>Privacy Policy</p>
+            <p>California Do Not Sell My Personal Information</p>
+            <p>Diversity, Equity and Inclusion at J.Crew Group</p>
+        </div>
+
+    </div>
+
+    <div id = "content_search">
+        <p>Like Being First?</p>
+        <p>Get can't miss style news, before everybody else.</p>
+        <div id = "search1">
+            <input type="text" placeholder = "Enter Your Search">
+            <button>SignUp</button>
+        </div>
+        
+    </div>
+</div>
+
+<div id = "contact">
+    <div id = "socialbar">
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-facebook-square"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-pinterest"></i>
+        <i class="fa-brands fa-youtube"></i>
+    </div>
+    <p>India</p>
+
+    <div id = "company_logo">
+        <img src = "https://static.subbly.me/fs/subbly/userFiles/giogelati/images/1391-madewell.png?v=1604565045"/>
+        <img src = "https://assets.website-files.com/5fc5bda0a87ecc70c09251a4/5fc99f3aa4ebe87026de2739_logo.png"/>
+    </div>
+
+    <p>© 2022 J.Crew</p>
+</div>`
+}
+
+
+function foot (){
+    return `
+    <div>
+        <i class="fa-brands fa-twitter"></i>
+        <p>@jcrew_help</p>
+    </div>
+    <div>
+        <i class="fa-solid fa-phone"></i>
+        <p>1 434 385 5775</p>
+    </div>
+    <div>
+        <i class="fa-solid fa-envelope"></i>
+        <p>Email Us</p>
+    </div>`
+}
+
+export { footer,foot };
