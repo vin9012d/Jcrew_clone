21f6c9a7c40380c8951586eeb445c53d0803a105
vin9012d <vin9012c@gmail.com>
2022-06-15 11:03:56 +0530

    payment gateway

---
+++ append.js
@@ -0,0 +1,29 @@
+let data=[
+    {image:"https://www.jcrew.com/s7-img-facade/BI574_PT1317_m?wid=416",price:100,quantity:1, size:"M",color:"Navy Blue" },
+    {image:"https://www.jcrew.com/s7-img-facade/BI574_PT1317_m?wid=416",price:150,quantity:2, size:"M",color:"Army green"}
+]
+
+data.forEach((ele)=>{
+let container=document.querySelector("#cart_datas")
+let box=document.createElement("div");
+let left=document.createElement("div");
+let right=document.createElement("div");
+
+let image=document.createElement("img");
+image.src=ele.image;
+let price=document.createElement("p")
+price.innerText=ele.price;
+let quantity=document.createElement("p")
+quantity.innerText=ele.quantity;
+let size=document.createElement("p")
+size.innerText=ele.size;
+let color=document.createElement("p")
+color.innerText=ele.color;
+let button=document.createElement("button");
+button.innerText="View More"
+left.append(image)
+right.append(price,quantity,size,color)
+box.append(left,right)
+container.append(box,button)
+
+})
