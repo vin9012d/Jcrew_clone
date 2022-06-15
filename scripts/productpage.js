65214729d76093df8e01d917886ad949823eb03c
kishan169 <kishansavaj1206@gmail.com>
2022-06-15 18:01:40 +0530

    add js file

---
+++ scripts/productpage.js
@@ -0,0 +1,10 @@
+// let product_data = JSON.parse(localStorage.getItem("product"));
+
+// document.getElementById("product_image").src = 
+// document.getElementById("product_name").innerText = 
+// document.getElementById("product_price").innerText = 
+// document.getElementById("Striked_price").innerText = 
+// document.getElementById("description").innerText = 
+
+
+
