d313c2d55a6928d1898d90cdf0cb1a5bf5dd33ae
vin9012d <vin9012c@gmail.com>
2022-06-16 10:45:24 +0530

    update

--- append.js
+++ append.js
@@ -1,6 +1,6 @@
 let data=[
-    {image:"https://www.jcrew.com/s7-img-facade/BI574_PT1317_m?wid=416",price:100,quantity:1, size:"M",color:"Navy Blue" },
-    {image:"https://www.jcrew.com/s7-img-facade/BI574_PT1317_m?wid=416",price:150,quantity:2, size:"M",color:"Army green"}
+    {image:"https://www.jcrew.com/s7-img-facade/BI574_PT1317_m?wid=416",price:100,quantity:1, size:"M",color:"Navy Blue", name:"product dredd" },
+    {image:"https://www.jcrew.com/s7-img-facade/BI574_PT1317_m?wid=416",price:150,quantity:2, size:"M",color:"Army green",name:"product crecc"}
 ]
 
 data.forEach((ele)=>{
@@ -11,6 +11,8 @@ let right=document.createElement("div");
 
 let image=document.createElement("img");
 image.src=ele.image;
+let name=document.createElement("p");
+name.innerText=ele.name;
 let price=document.createElement("p")
 price.innerText=ele.price;
 let quantity=document.createElement("p")
@@ -24,6 +26,6 @@ button.innerText="View More"
 left.append(image)
 right.append(price,quantity,size,color)
 box.append(left,right)
-container.append(box,button)
+container.append(name,box,button)
 
 })
