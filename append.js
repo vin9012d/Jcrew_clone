3bfa66ef550714c628e779c5e3a51c79d382cffb
vin9012d <vin9012c@gmail.com>
2022-06-17 01:20:32 +0530

    almost done

--- append.js
+++ append.js
@@ -6,26 +6,79 @@ let data=[
 data.forEach((ele)=>{
 let container=document.querySelector("#cart_datas")
 let box=document.createElement("div");
+box.setAttribute("class","box")
 let left=document.createElement("div");
 let right=document.createElement("div");
+right.setAttribute("class","right")
 
 let image=document.createElement("img");
 image.src=ele.image;
 let name=document.createElement("p");
 name.innerText=ele.name;
+let price_name=document.createElement("p");
+price_name.innerText="name:"
+let quantity_name=document.createElement("p");
+quantity_name.innerText="quantity:"
+let color_name=document.createElement("p");
+color_name.innerText="color:"
+let size_name=document.createElement("p");
+size_name.innerText="size:"
+let price_div=document.createElement("div");
 let price=document.createElement("p")
 price.innerText=ele.price;
+let quantity_div=document.createElement("div");
 let quantity=document.createElement("p")
 quantity.innerText=ele.quantity;
+let size_div=document.createElement("div");
 let size=document.createElement("p")
 size.innerText=ele.size;
+let color_div=document.createElement("div");
 let color=document.createElement("p")
 color.innerText=ele.color;
 let button=document.createElement("button");
 button.innerText="View More"
 left.append(image)
-right.append(price,quantity,size,color)
+price_div.append(price_name,price)
+
+quantity_div.append(quantity_name,quantity)
+
+size_div.append(size_name,size)
+
+
+color_div.append(color_name,color)
+
+right.append(price_div,quantity_div,size_div,color_div)
 box.append(left,right)
 container.append(name,box,button)
 
-})
+});
+
+function formData(event){
+    event.preventDefault();
+    console.log(agree.checked)
+  if(agree.checked==true){
+
+
+    let form =document.querySelector("#form");
+    let fname=form.fname.value;
+    let lname=form.lname.value;
+    let country=form.country.value;
+    let street=form.street.value;
+    let pin=form.pin.value;
+    let city=form.city.value;
+
+    let data={
+       names:`${fname} ${lname}`,
+       street:street,
+       city:`${city}, ${pin}`,
+       country:country,
+    }
+
+    localStorage.setItem("address",JSON.stringify(data))
+}
+else{
+    alert("Accept the terms and conditins")
+}
+
+}
+
---
+++ innerfunctionality.js
@@ -0,0 +1,74 @@
+//edit adress
+function edit(){
+    window.location.href="payment1.html"
+}
+
+//address apend
+let addressData=JSON.parse(localStorage.getItem("address"));
+document.querySelector("#name").innerText=addressData.names;
+
+document.querySelector("#street").innerText=addressData.street;
+
+document.querySelector("#city").innerText=addressData.city;
+document.querySelector("#country").innerText=addressData.country;
+//Paynow button click
+function paynow(event){
+//    console.log(event.target)
+timer()
+   let card_div=document.querySelector("#card_div");
+   let otp=document.querySelector("#otp");
+   card_div.style.display="none";
+   otp.style.display="block";
+
+   setTimeout(function(){
+ alert("Your one time password OTP is 78665")
+   },5000)
+}
+
+function resend(){
+    setTimeout(function(){
+        alert("Your one time password OTP is 78665")
+          },5000)
+}
+
+//submit payment
+function submit_otp(){
+    let otp=document.querySelector("#enter_otp").value;
+    if(otp=="78665"){
+        alert(`
+        Your order is placed sucessfully
+        Now You will be redirected to the homepage`)
+    }else
+    alert(`You have entered wrong otp`)
+}
+
+
+
+//timer
+function timer(){
+    let min_container=document.querySelector("#timer_min")
+    let sec_container=document.querySelector("#timer_seconds")
+let min=2;
+let sec=30;
+min_container.innerText=min;
+sec_container.innerText=sec;
+    let id=setInterval(function(){
+  sec=sec-1;
+  if(min==0 && sec==0){
+    alert("Your payment failed");
+    window.location.hred="#";
+    clearInterval(id)
+    
+ }
+ if(sec==0){
+    sec=60;
+    min=min-1;
+    min_container.innerText=min;
+ }
+
+ sec_container.innerText=sec;
+
+
+    },1000)
+
+}
