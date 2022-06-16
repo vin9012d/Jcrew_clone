54bc54faaf714ac43c7f54cacc2f80f76584aec8
kishan169 <kishansavaj1206@gmail.com>
2022-06-16 12:58:01 +0530

    css edit

--- components/navbar.js
+++ components/navbar.js
@@ -2,23 +2,23 @@ function navbar () {
     return `
     <div id ="options">
             <img id = "logo" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5KK9ADgSi2BO9dOLPH3epWfEeLe4m-RzqYPb7YLcjW7MArQu19_sXSS3jhXy807JJOg&usqp=CAU">
-            <section><a href = "https://www.google.com/">New</a></section>
-            <section>Women</section>
-            <section>Men</section>
-            <section>Kids</section>
-            <section>Swim</section>
-            <section>Cashmare</section>
-            <section>Home</section>
-            <section>Stories</section>
-            <section>Sale</section>
+            <div><a href = "https://www.google.com/">New</a></div>
+            <div>Women</div>
+            <div>Men</div>
+            <div>Kids</div>
+            <div>Swim</div>
+            <div>Cashmare</div>
+            <div>Home</div>
+            <div>Stories</div>
+            <div>Sale</div>
         </div>   
         
         <div id ="search">
             <input type = "text" placeholder ="Search j.crew">
             <i class="fa-light fa-magnifying-glass"></i>
-            <section>Sign In</section>
-            <section><i class="fa-light fa-heart"></i></section>
-            <section><i class="fa-light fa-bag-shopping"></i></section>
+            <div><a href = "#">Sign In</a></div>
+            <div><i class="fa-light fa-heart"></i></div>
+            <div><i class="fa-light fa-bag-shopping"></i></div>
 
         </div>  `
 }
